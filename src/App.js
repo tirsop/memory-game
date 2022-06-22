c0ab7cb77d49c8d06c5fc8a8c8c93b9c07981005
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-23 08:25:50 +0900

    added match prop to all cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 function App() {
@@ -35,15 +35,22 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {     // only execute code when we have the 2 choices
       if (choiceOne.src === choiceTwo.src) {
-        console.log("match")
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
         resetTurn()
       } else {
-        console.log("dont matchbro");
         resetTurn()
       }
     }
   }, [choiceOne, choiceTwo])
-
+  console.log(cards);
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
