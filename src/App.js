fa280b736168bea852e80213e3e3ff48bb301a34
Tirso Pascual <tpascualgarcia@gmail.com>
2022-09-06 19:11:20 +0900

    add card style

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import './App.css'
 import SingleCard from './components/SingleCard'
 
 const countries = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "vienna", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azrbaijan", capital: "baku", matched: false },
+  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/austria.png", country: "austria", capital: "vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
 ]
 
 const capitals = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "Vienna", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azrbaijan", capital: "baku", matched: false },
+  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/austria.png", country: "austria", capital: "Vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
 ]
 
 
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -10,15 +10,16 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   return (
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
-        <div className="front">
-          <img src={card.src} alt="card front" />
-          <p className="">{card.country}</p>
+        <div className="front" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${card.src})` }}>
+          {/* <img src={card.src} alt="card front" /> */}
+          <h3 className="">{card.country[0].toUpperCase()}{card.country.slice(1)}</h3>
+          {/* <p className="population">Poplulation: {card.poplulation}</p> */}
         </div>
-        <img src="/img/cover.png"
+        <img src="/img/bahamas.png"
           className="back"
           alt="card back"
           onClick={handleClick} />
       </div>
-    </div>
+    </div >
   )
 }
