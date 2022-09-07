c84c43fa413ce33069c002d758c9598407cdbc3f
Tirso Pascual <tpascualgarcia@gmail.com>
2022-09-07 16:18:41 +0900

    change card style

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -11,10 +11,10 @@ export default function SingleCard({ card, handleChoice, flipped, disabled }) {
     <div className="card">
       <div className={flipped ? "flipped" : ""}>
         <div className="front" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${card.src})` }}>
-          {/* <img src={card.src} alt="card front" /> */}
-          <h3 className="">{card.country[0].toUpperCase()}{card.country.slice(1)}</h3>
-          {/* <p className="population">Poplulation: {card.poplulation}</p> */}
+          {card.isCountry && <h3 className="">{card.country[0].toUpperCase()}{card.country.slice(1)}</h3>}
+          {!card.isCountry && <h3 className="">{card.capital[0].toUpperCase()}{card.capital.slice(1)}</h3>}
         </div>
+
         <img src={`/img/mapamundi/image_part_${card.id}.jpg`}
           className="back"
           alt="card back"
