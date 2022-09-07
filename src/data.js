0781f372ab7d7d0f1b97d8853709a0814cfa7f28
Tirso Pascual <tpascualgarcia@gmail.com>
2022-09-07 15:30:23 +0900

    add new countries to the game

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 
 const countries = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "vienna", poplulation: "38.9million", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/austria.png", country: "austria", capital: "vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
 ]
 
 const capitals = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "Vienna", poplulation: "38.9million", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/austria.png", country: "austria", capital: "Vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/countries/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
 ]
 
 export { countries, capitals }
