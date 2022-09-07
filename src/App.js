834f7275d3aae302a3a6d013c378b350fa658eba
Tirso Pascual <tpascualgarcia@gmail.com>
2022-09-07 15:09:00 +0900

    put data about countries in different file

--- src/App.js
+++ src/App.js
@@ -3,26 +3,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 // import background from './canvas.js'
-
-const countries = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "vienna", poplulation: "38.9million", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
-]
-
-const capitals = [
-  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
-  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
-  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
-  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
-  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
-  { "src": "/img/austria.png", country: "austria", capital: "Vienna", poplulation: "38.9million", matched: false },
-  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
-]
+import { countries, capitals } from './data.js'
 
 
 function App() {
@@ -31,6 +12,8 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
   const [disabled, setDisabled] = useState(false) // to prevent the user to make lots of clicks
+
+
 
   // function that return X random elements of an array
   const random = (array, elements) => {
@@ -43,6 +26,8 @@ function App() {
     }
     return result
   }
+
+
 
   // func that randomly picks 6 countryCards, find their capitalCards, shuffle them and puts an id on each
   const shuffleCards = useCallback(() => {
@@ -59,17 +44,20 @@ function App() {
     for (let i = 0; i < shuffledCards.length; i++) {
       shuffledCards[i] = { ...shuffledCards[i], id: i + 1 }
     }
-
     setChoiceOne(null) // so choices are reset when pressing NewGame
     setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
   }, [])
 
+
+
   // user choices and is assigned to the first or second choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
+
+
 
   // compare 2 selected cards
   useEffect(() => {
@@ -93,12 +81,15 @@ function App() {
   }, [choiceOne, choiceTwo])
 
 
+
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
     setDisabled(false)
   }
+
+
 
   // start a new game as soos as user enters the page
   useEffect(() => {
---
+++ src/data.js
@@ -0,0 +1,22 @@
+
+const countries = [
+  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/austria.png", country: "austria", capital: "vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
+]
+
+const capitals = [
+  { "src": "/img/afghanistan.png", country: "afghanistan", capital: "kabul", poplulation: "38.9million", matched: false },
+  { "src": "/img/albania.png", country: "albania", capital: "tirana", poplulation: "38.9million", matched: false },
+  { "src": "/img/algeria.png", country: "algeria", capital: "algiers", poplulation: "38.9million", matched: false },
+  { "src": "/img/armenia.png", country: "armenia", capital: "yerevan", poplulation: "38.9million", matched: false },
+  { "src": "/img/australia.png", country: "australia", capital: "canberra", poplulation: "38.9million", matched: false },
+  { "src": "/img/austria.png", country: "austria", capital: "Vienna", poplulation: "38.9million", matched: false },
+  { "src": "/img/azerbaijan.png", country: "azerbaijan", capital: "baku", poplulation: "38.9million", matched: false },
+]
+
+export { countries, capitals }
